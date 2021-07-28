312e371bd24c0e9b1a63f1e17cc4f63d62047ca1
Bernardo Coutinho <bernardocdr@gmail.com>
2021-07-28 18:51:58 -0300

    (1.4.1)Exercícios 1 e 2 resolvidos. :rocket:

---
+++ modulo1_fundamentos/bloco04_javascript_e_logica/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,104 @@
+
+//Exercício 1
+
+  /*
+    let a = 2;
+    let b = 4;
+
+    console.log('Soma: '+(a+b));
+    console.log('Subtração: '+(a-b));
+    console.log('Multiplicação: '+ (a*b));
+    console.log('Divisão: '+(a/b));
+    console.log('Módulo: '+(a%b));
+  */
+
+
+//Exercício 2
+//ERRO: se os dois números apresentarem o mesmo valor, mostra a mensagem "'b' é maior que 'a'"
+
+  /*
+    let a = 10;
+    let b = 11;
+
+    if (a > b) {
+      console.log("'a' é maior que 'b'");
+    } 
+
+    else {
+      console.log("'b' é maior que 'a'");};
+  */
+
+
+//Exercício 3
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados. 
+
+
+
+
+//Exercício 4
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. 
+
+
+
+
+//Exercício 5
+//Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. 
+
+//Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+
+
+
+
+//Exercício 6
+//Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+//Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+
+//Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+
+//Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+//Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+
+
+
+//Exercício 7
+
+//Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+//Porcentagem >= 90 -> A
+//Porcentagem >= 80 -> B
+//Porcentagem >= 70 -> C
+//Porcentagem >= 60 -> D
+//Porcentagem >= 50 -> E
+//Porcentagem < 50 -> F
+//O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. 
+
+
+
+//Exercício 8
+//Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+
+//Bonus: use somente um if .
+
+
+
+
+
+//Exercício 9
+
+//Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+
+//Bonus: use somente um if .
+
+
+
+
+//Exercício 10
+
+
+
+
+//Exercício 11
