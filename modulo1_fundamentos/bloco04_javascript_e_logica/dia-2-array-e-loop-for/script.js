66439f6361ee9d52b54a6ea38c34c01047b04efb
Bernardo Coutinho <bernardocdr@gmail.com>
2021-07-29 18:25:04 -0300

    1.4.2 - Exercícios 1 a 3 concluídos

--- modulo1_fundamentos/bloco04_javascript_e_logica/dia-2-array-e-loop-for/script.js
+++ modulo1_fundamentos/bloco04_javascript_e_logica/dia-2-array-e-loop-for/script.js
@@ -1,2 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaTotal = 0;
+
 console.log(numbers);
+
+for(let index = 0; index < numbers.length; index += 1){
+  somaTotal += numbers[index];  
+}
+
+console.log("A soma total dos números é "+ somaTotal);
+
+console.log("A média aritmética é "+ somaTotal/numbers.length );
