8f1f4af001040f82d0430bfa0577756c7dec6602
Bernardo Coutinho <bernardocdr@gmail.com>
2021-07-29 18:56:33 -0300

    1.4.2 - Exercicios 4 e 5 concluídos.

--- modulo1_fundamentos/bloco04_javascript_e_logica/dia-2-array-e-loop-for/script.js
+++ modulo1_fundamentos/bloco04_javascript_e_logica/dia-2-array-e-loop-for/script.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaTotal = 0;
+let numeroMaior= 0;
+let numeroNovo = 0;
 
 console.log(numbers);
 
@@ -7,6 +9,28 @@ for(let index = 0; index < numbers.length; index += 1){
   somaTotal += numbers[index];  
 }
 
+
+for(let indexMaior = 0; indexMaior < numbers.length; indexMaior += 1){
+  numeroNovo = numbers[indexMaior];
+  if (numeroNovo>numeroMaior){
+    numeroMaior = numeroNovo;
+    console.log(numeroMaior)
+  }
+}
+
+
+mediaAritmetica = somaTotal/numbers.length;
+
 console.log("A soma total dos números é "+ somaTotal);
 
-console.log("A média aritmética é "+ somaTotal/numbers.length );
+console.log("A média aritmética é "+ mediaAritmetica);
+
+if (mediaAritmetica > 20) {
+  console.log("Valor maior que 20 ");
+}
+else
+{
+  console.log("Valor menor ou igual a 20");
+}
+
+console.log("O maior número da lista é "+ numeroMaior);
