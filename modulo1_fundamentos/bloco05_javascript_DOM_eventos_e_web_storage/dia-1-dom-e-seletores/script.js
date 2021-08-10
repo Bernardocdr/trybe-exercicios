29e2a7ea0000af59edb886e1512053173d272736
Bernardo Coutinho <bernardocdr@gmail.com>
2021-08-10 12:04:21 -0300

    1.5.1 - Exercícios Concluídos! :rocket:

--- modulo1_fundamentos/bloco05_javascript_DOM_eventos_e_web_storage/dia-1-dom-e-seletores/script.js
+++ modulo1_fundamentos/bloco05_javascript_DOM_eventos_e_web_storage/dia-1-dom-e-seletores/script.js
@@ -21,20 +21,37 @@ exercicio02("rgb(76,164,109)");
 
 // Crie uma função que mude a cor do quadrado vermelho para branco.
 
+function exercicio03(cor){  
+  square = document.querySelector(".main-content .center-content");
+  square.style.backgroundColor = cor;
+  console.log("Exercício 03: "+cor);
+}
 
+exercicio03("white");
 
 // Crie uma função que corrija o texto da tag <h1>.
 
+function exercicio04(texto){  
+  titulo = document.querySelector("h1");
+  titulo.innerText = texto;
+  console.log("Exercício 04: "+texto);
+}
+
+exercicio04("Exercício 5.1 - JavaScript");
 
 // Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
-/*
-document.getElementById()
-document.getElementsByClassName()
-document.getElementsByTagName()
-*/
+function exercicio05(){  
+  document.querySelector("p").innerHTML = document.querySelector("p").innerHTML.toUpperCase();
+  console.log("Exercício 05: Maiúsculo: "+ document.querySelector("p").innerText);
+}
+
+exercicio05();
 // Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-/*
-document.getElementById()
-document.getElementsByClassName()
-document.getElementsByTagName()
-*/
+function exercicio06(){  
+  const text = document.querySelectorAll("p")
+  console.log("Exercício 06: ")
+  for (i=0; i<text.length; i += 1){
+    console.log(text[i].innerText)
+  }
+}
+exercicio06();
